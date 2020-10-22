3d0996d563229e319afa3b4e5d5cf48ac4100bb8
Code 101 <code101@MacBook-Pro-de-Kevin.local>
2020-10-22 18:43:04 +0200

    add pwa

---
+++ sw.js
@@ -0,0 +1,28 @@
+const version = "1.0.0";
+const cacheName = `blackscreen-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/style.css`
+      ])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
